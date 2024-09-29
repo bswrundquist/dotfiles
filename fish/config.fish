6700ab235d1b57783d9e329ac29751e26e9ff639
bswr <bswrundquist@gmail.com>
2024-09-29 01:34:16 +0000

    chore: Remove everything for later organization

--- fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-end
-source ~/.config/fish/aliases.fish
-
-fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/bswr/.local/bin
--- fish/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-  alias --save w-ee="cd ~/workspace/eagleeye"
-  alias --save dev-ee="tmuxp load ~/workspace/eagleeye/devlayout.yaml"
-    # Commands to run in interactive sessions can go here
-end
-
