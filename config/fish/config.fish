1382f1b3d78d660d13bb2028821c3156a0e8b905
bswr <bswrundquist@gmail.com>
2024-09-29 02:52:58 +0000

    feat(fish): Install and configure fish and set as default terminal

---
+++ config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+end
+source ~/.config/fish/aliases.fish
+
+fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/bswr/.local/bin
