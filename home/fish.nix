a17b7ad7ec6bb6814e3cab6827187e5e9ff1c2f7
bswr <bswrundquist@gmail.com>
2025-01-04 07:43:42 -0700

    todo: Needs work to get going

--- home/fish.nix
+++ home/fish.nix
@@ -3,59 +3,51 @@
   programs.fish = {
     # disabled due to some issue with needing opengl version 3.3
     enable = true;
-    plugins = [
-      {
-        name = "fisher";
-        src = "jorgebucaran/fisher";
-      }
-      {
-        name = "fzf-fish";
-        src = "PatrickF1/fzf.fish";
-      }
-      {
-        name = "z";
-        src = "jethrokuan/z";
-      }
-      {
-        name = "zoxide";
-        src = "ajeetdsouza/zoxide";
-      }
-      {
-        name = "fzf";
-        src = "jethrokuan/fzf";
-      }
-      {
-        name = "exa";
-        src = "ogham/exa";
-      }
-      {
-        name = "bat";
-        src = "sharkdp/bat";
-      }
-      {
-        name = "lsd";
-        src = "Peltoche/lsd";
-      }
-      {
-        name = "starship";
-        src = "starship/starship";
-      }
-    ];
-    settings = {
-      font_size = 14;
-      font_family = "JetBrainsMono";
-      copy_on_select = "yes";
-      cursor_shape = "block";
-      cursor_blink_interval = 0;
-      enable_audio_bell = "no";
-      shell = "fish";
-      editor = "nvim";
-      window_padding_width = 5;
-      tab_title_template = "{index}";
-      tab_bar_style = "powerline";
-      tab_powerline_style = "angled";
-      enabled_layouts = "vertical";
-    };
+    # plugins = [
+    #   {
+    #     name = "fisher";
+    #     src = "jorgebucaran/fisher";
+    #   }
+    #   {
+    #     name = "fzf-fish";
+    #     src = "PatrickF1/fzf.fish";
+    #   }
+    #   {
+    #     name = "z";
+    #     src = "jethrokuan/z";
+    #   }
+    #   {
+    #     name = "zoxide";
+    #     src = "ajeetdsouza/zoxide";
+    #   }
+    #   {
+    #     name = "fzf";
+    #     src = "jethrokuan/fzf";
+    #   }
+    #   # {
+    #   #   name = "exa";
+    #   #   src = "ogham/exa";
+    #   # }
+    #   {
+    #     name = "starship";
+    #     src = "starship/starship";
+    #   }
+    # ];
+    # settings = {
+    #   font_size = 14;
+    #   font_family = "JetBrainsMono";
+    #   copy_on_select = "yes";
+    #   cursor_shape = "block";
+    #   cursor_blink_interval = 0;
+    #   enable_audio_bell = "no";
+    #   shell = "fish";
+    #   editor = "nvim";
+    #   window_padding_width = 5;
+    #   tab_title_template = "{index}";
+    #   tab_bar_style = "powerline";
+    #   tab_powerline_style = "angled";
+    #   enabled_layouts = "vertical";
+    # };
     shellInit = ''
       # Disable greeting
       set fish_greeting
