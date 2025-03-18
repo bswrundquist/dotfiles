411a4f286af080047da404edfc7b55f901f88f19
bswr <bswrundquist@gmail.com>
2025-03-18 06:32:07 -0600

    feat: Add python 3.12 with some base packages

--- home/python312.nix
+++ home/python312.nix
@@ -3,7 +3,14 @@
 {
   home.packages = with pkgs; [
     (python312.withPackages (ps: with ps; [
+      gcsfs
       numpy
+      polars
+      pandas
+      pyarrow
+      matplotlib
+      seaborn
+      scikit-learn
     ]))
   ];
 }
