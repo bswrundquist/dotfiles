2c97ca55778205885b83ec808b816345b1ba8dce
bswr <bswrundquist@gmail.com>
2025-03-18 10:54:05 -0600

    chore: Make home username and directory use input from flake.nix

--- home/home.nix
+++ home/home.nix
@@ -1,6 +1,20 @@
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 
 {
+  imports = [
+    ./zsh.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = username;
+  home.homeDirectory = "/Users/${username}";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  # home.stateVersion = "23.11"; # Please read the comment before changing.
+
   home.sessionVariables.PATH = "/opt/homebrew/bin:/opt/homebrew/sbin:$PATH";
 }
 
