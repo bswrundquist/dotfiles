5d8354da96a018b0602098ad91abbf8e54dd2fd9
bswr <bswrundquist@gmail.com>
2025-03-18 10:19:17 -0600

    chore: Remove global config so can interpolate username and useremail

--- home/git.nix
+++ home/git.nix
@@ -1,66 +1,50 @@
+{ config, lib, pkgs, username, useremail, ... }:
+
 {
-  lib,
-  username,
-  useremail,
-  ...
-}: {
-  # `programs.git` will generate the config file: ~/.config/git/config
-  # to make git use this config file, `~/.gitconfig` should not exist!
-  #
-  #    https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
-  home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
-    rm -f ~/.gitconfig
-  '';
-
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-
-    # TODO replace with your own name & email
-    userName = username;
-    userEmail = useremail;
-
-    includes = [
-      {
-        # use diffrent email & name for work
-        path = "~/work/.gitconfig";
-        condition = "gitdir:~/work/";
-      }
-    ];
-
-    extraConfig = {
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      pull.rebase = true;
-    };
-
-    # signing = {
-    #   key = "xxx";
-    #   signByDefault = true;
-    # };
-
-    delta = {
+  home-manager.users.${username} = {
+    programs.git = {
       enable = true;
-      options = {
-        features = "side-by-side";
+      userName = useremail;
+      userEmail = useremail;
+      extraConfig = {
+        init.defaultBranch = "main";
+        color.ui = "auto";
+        color.diff = "auto";
+        color.status = "auto";
+        color.branch = "auto";
+        color.interactive = "auto";
+        log.abbrevCommit = true;
+        log.decorate = true;
+        log.graph = true;
+        log.date = "relative";
+        status.showUntrackedFiles = "all";
+        push.default = "simple";
+        pull.rebase = true;
+        pull.ff = "only";
+        diff.tool = "vimdiff";
+        diff.renames = "copies";
+        diff.colorMoved = "zebra";
+        merge.tool = "vimdiff";
+        merge.conflictStyle = "diff3";
+        merge.ff = false;
+        core.editor = "nvim";
+        core.pager = "less -FX";
+        core.autocrlf = "input";
+        core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
+        pretty.graph = "%C(auto)%h%d %s %C(blue)%an, %C(cyan)%ar";
+        pretty.format = "%C(auto)%h %s %C(blue)%an%C(reset)";
+        credential.helper = "cache";
+        commit.gpgSign = false;
+        gpg.program = "gpg";
+        pager.log = true;
+        pager.diff = true;
+        pager.status = true;
+        pager.show = true;
+        mergetool.prompt = false;
+        mergetool.keepBackup = false;
+        push.autoSetupRemote = true;
+        rebase.autoSquash = true;
       };
     };
-
-    aliases = {
-      # common aliases
-      br = "branch";
-      co = "checkout";
-      st = "status";
-      ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
-      ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
-      cm = "commit -m";
-      ca = "commit -am";
-      dc = "diff --cached";
-      amend = "commit --amend -m";
-
-      # aliases for submodule
-      update = "submodule update --init --recursive";
-      foreach = "submodule foreach";
-    };
   };
-}
+} 
