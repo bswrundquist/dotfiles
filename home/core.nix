e18134d428f9a2aa6db1745326909d94dec37d71
bswr <bswrundquist@gmail.com>
2025-03-18 22:48:55 -0600

    chore: Add base gcc, rust

--- home/core.nix
+++ home/core.nix
@@ -11,6 +11,9 @@
     p7zip
 
     # utils
+    rustc
+    gcc
+    cargo
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processer https://github.com/mikefarah/yq
