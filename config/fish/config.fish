1853ec978367e827074a7ee68f02dc34503f8769
bswr <bswrundquist@gmail.com>
2025-04-01 08:26:13 -0600

    fix: Simple fish config fix

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
 end
 source ~/.config/fish/aliases.fish
 
-fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/bswr/.local/bin
+# Make sure Nix paths are first in the PATH
+fish_add_path --prepend --move $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin
+fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin $HOME/.local/bin
