fdbfb746f373f592467d70963a45ed8a286dfc5e
bswr <bswrundquist@gmail.com>
2025-09-15 17:26:04 -0600

    chore(nvim): Keymap esc for getting out of terminal edit mode

--- config/nvim/lua/polish.lua
+++ config/nvim/lua/polish.lua
@@ -3,19 +3,17 @@
 -- fit in the normal config locations above can go here
 
 -- Make popups disappear faster
-vim.opt.updatetime = 300  -- Faster CursorHold events (default is 4000ms)
-vim.opt.timeoutlen = 300  -- Time to wait for a mapped sequence to complete (ms)
+vim.opt.updatetime = 300 -- Faster CursorHold events (default is 4000ms)
+vim.opt.timeoutlen = 300 -- Time to wait for a mapped sequence to complete (ms)
 
 -- Configure popup menu behavior
-vim.opt.pumheight = 10  -- Maximum number of items to show in popup menu
-vim.opt.pumblend = 10   -- Slight transparency for popup menu
+vim.opt.pumheight = 10 -- Maximum number of items to show in popup menu
+vim.opt.pumblend = 10 -- Slight transparency for popup menu
 
 -- Close documentation windows with Escape
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"help", "lspinfo", "man", "qf"},
-  callback = function()
-    vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = true, silent = true })
-  end,
+  pattern = { "help", "lspinfo", "man", "qf" },
+  callback = function() vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = true, silent = true }) end,
 })
 
 -- Set up custom filetypes
@@ -32,4 +30,5 @@ vim.filetype.add {
 }
 
 -- Add keymap for opening notes.md
-vim.keymap.set('n', '<leader>n', ':e ~/notes.md<CR>', { silent = true })
+vim.keymap.set("n", "<leader>n", ":e ~/notes.md<CR>", { silent = true })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
